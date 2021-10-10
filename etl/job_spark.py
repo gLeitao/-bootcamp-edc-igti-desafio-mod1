360374fca0cecdf31a12c8d7545f11572b75135f
gLeitao <geovani.leitao@gmail.com>
2021-10-10 16:36:02 -0300

    subindo infra desafio mod1

---
+++ etl/job_spark.py
@@ -0,0 +1,23 @@
+from pyspark.sql.functions import mean, max, min, col, count
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder.appName("ExerciseSpark")
+    .getOrCreate()
+)
+
+enem = (spark.read
+            .format("csv")
+            .option("header", True)
+            .option("inferSchema", True)
+            .option("delimiter", ";")
+            .load("s3://datalake-geovani-igti-edc-desafio-mod1-tf/raw/")
+)
+
+(
+    enem
+    .write
+    .mode("overwrite")
+    .format("parquet")
+    .save("s3://datalake-geovani-igti-edc-desafio-mod1-tf/staging/")
+)
